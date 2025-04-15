973a3333b1c0e8376df81c40e347f2f3d291c244
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2025-04-15 19:12:37 +0500

    Update ViewSalary.jsx

--- src/components/salary/ViewSalary.jsx
+++ src/components/salary/ViewSalary.jsx
@@ -18,7 +18,7 @@ const ViewSalary = () => {
     useEffect(() => {
         const fetchSalaries = async () => {
             try {
-                const response = await axios.get(`http://localhost:3000/api/salary/${id}/${user.role}`, {
+                const response = await axios.get(`https://employee-management-backend-flame.vercel.app/api/salary/${id}/${user.role}`, {
                     headers: {
                         "Authorization": `Bearer ${localStorage.getItem("token")}`,
                     }
