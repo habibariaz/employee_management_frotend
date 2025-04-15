45e76ff2ba2d7060cf6d0541f7cb78c87b2871be
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2025-04-15 19:05:42 +0500

    Update AddEmployee.jsx

--- src/components/employee/AddEmployee.jsx
+++ src/components/employee/AddEmployee.jsx
@@ -40,7 +40,7 @@ const AddEmployee = () => {
         })
 
         try {
-            const response = await axios.post("http://localhost:3000/api/employees/addEmployee",
+            const response = await axios.post("https://employee-management-backend-flame.vercel.app/api/employees/addEmployee",
                 formDataObj, {
                 headers: {
                     "Authorization": `Bearer ${localStorage.getItem("token")}`
