b3c49948c02bfc48a1f8ae4bf43dafce74faf116
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2025-04-15 19:15:00 +0500

    Update EmployeeHelper.jsx

--- src/utils/EmployeeHelper.jsx
+++ src/utils/EmployeeHelper.jsx
@@ -42,7 +42,7 @@ export const columns = [
 export const fetchDepartments = async () => {
     let departments
     try {
-        const response = await axios.get("http://localhost:3000/api/department", {
+        const response = await axios.get("https://employee-management-backend-flame.vercel.app/api/department", {
             headers: {
                 "Authorization": `Bearer ${localStorage.getItem("token")}`
             }
@@ -61,7 +61,7 @@ export const fetchDepartments = async () => {
 export const getEmployees = async (id) => {
     let employees
     try {
-        const response = await axios.get(`http://localhost:3000/api/employees/department/${id}`, {
+        const response = await axios.get(`https://employee-management-backend-flame.vercel.app/api/employees/department/${id}`, {
             headers: {
                 "Authorization": `Bearer ${localStorage.getItem("token")}`
             }
