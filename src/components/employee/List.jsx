20ea5808fd0657e09df572e96e66d91a00b53fd4
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2025-04-15 19:07:32 +0500

    Update List.jsx

--- src/components/employee/List.jsx
+++ src/components/employee/List.jsx
@@ -15,7 +15,7 @@ const List = () => {
         const fetchEmployees = async () => {
             setEmpLoading(true)
             try {
-                const response = await axios.get("http://localhost:3000/api/employees", {
+                const response = await axios.get("https://employee-management-backend-flame.vercel.app/api/employees", {
                     headers: {
                         "Authorization": `Bearer ${localStorage.getItem("token")}`,
                     }
@@ -31,7 +31,7 @@ const List = () => {
                         dep_name: emp.department.dep_name,
                         name: emp.userId.name,
                         dob: new Date(emp.dob).toLocaleDateString(),
-                        profileImage: <img width={40} className='rounded-full' src={`http://localhost:3000/${emp.userId.profileImage}`} />,
+                        profileImage: <img width={40} className='rounded-full' src={`https://employee-management-backend-flame.vercel.app/${emp.userId.profileImage}`} />,
                         action: <EmployeeButtons id={emp._id} />
                     }));
 
