523f44492b1def2f64e62da2a84ef84c462a7a49
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2025-04-15 18:59:15 +0500

    Update AdminSummary.jsx

--- src/components/dashboard/AdminSummary.jsx
+++ src/components/dashboard/AdminSummary.jsx
@@ -10,7 +10,7 @@ const AdminSummary = () => {
     useEffect(() => {
         const fetchSummary = async () => {
             try {
-                const summary = await axios.get("http://localhost:3000/api/dashboard/summary", {
+                const summary = await axios.get("https://employee-management-backend-flame.vercel.app/api/dashboard/summary", {
                     headers: {
                         "Authorization": `Bearer ${localStorage.getItem('token')}`
                     }
