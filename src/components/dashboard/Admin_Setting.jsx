46dbb434c3be361f9440fc3f976b1ec8a848f8a7
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2025-04-15 18:58:45 +0500

    Update Admin_Setting.jsx

--- src/components/dashboard/Admin_Setting.jsx
+++ src/components/dashboard/Admin_Setting.jsx
@@ -20,7 +20,7 @@ const Admin_Setting = () => {
         e.preventDefault()
 
         try {
-            const response = await axios.put("http://localhost:3000/api/setting/change-password", setting,
+            const response = await axios.put("https://employee-management-backend-flame.vercel.app/api/setting/change-password", setting,
                 {
                     headers: {
                         "Authorization": `Bearer ${localStorage.getItem("token")}`
