761b1ffb3bab1989393005d5061d1750bff17101
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2025-04-15 19:13:14 +0500

    Update authContext.jsx

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -94,7 +94,7 @@ const AuthProvider = ({ children }) => {
             try {
                 const token = localStorage.getItem("token");
                 if (token) {
-                    const response = await axios.get("http://localhost:3000/api/auth/verify", {
+                    const response = await axios.get("https://employee-management-backend-flame.vercel.app/api/auth/verify", {
                         headers: {
                             "Authorization": `Bearer ${token}`
                         }
